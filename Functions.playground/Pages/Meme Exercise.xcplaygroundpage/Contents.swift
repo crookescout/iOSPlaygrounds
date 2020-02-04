2d76e57e81da02d2ec6fed50cb0aa7b3982cc32b
scout <scout.crooke@ssfs.org>
2020-02-04 14:28:51 -0500

    finished functions

---
+++ Functions.playground/Pages/Meme Exercise.xcplaygroundpage/Contents.swift
@@ -0,0 +1,68 @@
+/*:
+ ## Functional Meme
+ 
+ Think of a fun song or meme that you've heard or seen recently. Try to think of one with repetitive parts. For example:
+ 
+ - A song with a repeated chorus.
+ - A meme that repeats a word or phrase.
+ - A song that repeats a line over and over.
+ 
+ Write out the song or meme using `print` statements, then look for patterns and repetition and choose groups of lines to combine into functions. This is a free-form exercise, so you can do whatever you like.
+*/
+
+func downByTheBay() {
+    print("Down by the bay")
+}
+
+func watermelonsGrow() {
+    print("Where the watermelons grow")
+}
+
+func backToMyHome() {
+    print("Back to my home")
+}
+
+func dareNotGo() {
+    print("I dare not go")
+}
+
+func butIfIDo() {
+    print("But if I do")
+}
+
+func myMotherWillSay() {
+    print("My mother will say")
+}
+
+func haveYouSeengoat() {
+    print("Have you seen a goat that lives by the boat")
+}
+
+func VerseOne() {
+    downByTheBay()
+    downByTheBay()
+    watermelonsGrow()
+    watermelonsGrow()
+    backToMyHome()
+    backToMyHome()
+    dareNotGo()
+    dareNotGo()
+}
+
+func VerseTwo() {
+    butIfIDo()
+    butIfIDo()
+    myMotherWillSay()
+    myMotherWillSay()
+    haveYouSeengoat()
+    downByTheBay()
+}
+
+VerseOne()
+VerseTwo()
+
+
+
+//: Next, make the meme your own.
+//:
+//: [Previous](@previous)  |  page 11 of 12  |  [Next: Personal Meme](@next)
