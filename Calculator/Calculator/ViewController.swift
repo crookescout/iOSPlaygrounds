ff4391eb7b13714a0dec95f429b5a21337da9297
scout <scout.crooke@ssfs.org>
2020-02-06 08:26:03 -0500

    finished calculator

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Scout on 2/4/20.
+//  Copyright © 2020 Scout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
