3d76ad19e9828a26ad42ecc4a944a9544f14eadb
scout <scout.crooke@ssfs.org>
2020-02-13 14:47:41 -0500

    worked on traffic light

---
+++ TrafficLights/TrafficLights/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TrafficLights
+//
+//  Created by Scout on 2/13/20.
+//  Copyright © 2020 Scout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func returnToFirstScreen(unwindSegue: UIStoryboardSegue){
+        
+    }
+}
+
