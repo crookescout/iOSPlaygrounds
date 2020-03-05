a5cc6f59488ace12ff814a0f3e695ed39bd72ab0
scout <scout.crooke@ssfs.org>
2020-03-05 11:56:26 -0500

    finished Arrays and Loops

---
+++ ArraysAndLoops.playground/Pages/Exercise-Karaoke Host.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+/*:
+ ## Exercise: Karaoke Host
+
+ You have a friend who loves singing karaoke with a big group of people. The karaoke singers add songs they’d like to sing to a list and the karaoke host calls out the songs one by one. 
+ 
+ Your friend and has asked you to write software to help manage the song list.
+
+ - callout(Exercise):
+ Create an empty array to hold song titles as strings, and use the `append` method to add three or four songs one at a time.
+ */
+var songs = [String]()
+songs.append("DHL")
+songs.append("The Morning")
+songs.append("Bound 2")
+songs.append("See You Again")
+
+
+/*:
+ - callout(Exercise):
+ One enthusiastic singer wants to add three songs at once. Create an array holding this one singer's song list and use the `+=` operator to append their whole list to the end of the group's song list.
+ */
+songs += ["Feel No Ways", "3005", "Selfish"]
+
+
+/*:
+ - callout(Exercise):
+ Write a `for…in` loop and, for every song title in the array, print an encouraging announcement to let the next singer know that it's their turn.
+ */
+for song in songs {
+    print("Alright, let's welcome our next singer on to stage to sing \(song)!")
+}
+
+
+/*:
+ - callout(Exercise):
+ After the loop has called everyone up to sing, use the `removeAll` method on the song list to clear out all the past songs.
+ */
+songs.removeAll()
+
+
+//: [Previous](@previous)  |  page 14 of 17  |  [Next: Exercise: Counting Votes](@next)
