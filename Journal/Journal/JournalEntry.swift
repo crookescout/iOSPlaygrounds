e0f0b055fb3bedeb1ab0177c390eeddb7d6ee6ae
scout <scout.crooke@ssfs.org>
2020-03-12 09:21:50 -0400

    moving to laptop

---
+++ Journal/Journal/JournalEntry.swift
@@ -0,0 +1,17 @@
+//
+//  JournalEntry.swift
+//  Journal
+//
+//  Created by Scout on 3/11/20.
+//  Copyright © 2020 Scout. All rights reserved.
+//
+
+import Foundation
+
+struct JournalEntry {
+    
+    let date: Date
+    let contents: String
+    let dateFormatter = DateFormatter()
+    
+}
