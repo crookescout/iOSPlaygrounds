e679dce222b6b0f5b010849febf5ff14fb70f179
Scout Crooke <scoutcrooke@Scouts-MBP.fios-router.home>
2020-04-21 14:24:39 -0400

    Worked on my app

---
+++ Card Match/Card Match/CardCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CardCollectionViewCell.swift
+//  Card Match
+//
+//  Created by Scout Crooke on 4/21/20.
+//  Copyright © 2020 Scout Crooke. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    
+}
--- Card Match/Card Match/CardModel.swift
+++ Card Match/Card Match/CardModel.swift
@@ -10,41 +10,41 @@ import Foundation
 
 class CardModel {
     
-//    func getCards() -> [Card] {
-//        
-//        //Declare an array to store the generated cards
-//        var generatedCardsArray = [Card]()
-//        
-//        //Randomly generate pairs fo cards
-//        for _ in 1...8 {
-//            
-//            //Get a random number
-//            let randomNumber = arc4random_uniform(13) + 1
-//            
-//            //Log the number
-//            print(randomNumber)
-//            
-//            //Create the first card object
-//            let cardOne = Card()
-//            cardOne.imageName = "card\(randomNumber)"
-//            
-//            generatedCardsArray.append(cardOne)
-//            
-//            //Create the second card object
-//            let cardTwo = Card()
-//            cardTwo.imageName = "card\(randomNumber)"
-//            
-//            generatedCardsArray.append(cardTwo)
-//            
-//            //OPTIONAL: Make it so we only have unique pairs of cards
-//            
-//        }
-//        
-//        //TODO: Randomize the array
-//        
-//        
-//        //Return the arry
-//        return generatedCardsArray
-//    }
+    func getCards() -> [Card] {
+        
+        //Declare an array to store the generated cards
+        var generatedCardsArray = [Card]()
+        
+        //Randomly generate pairs fo cards
+        for _ in 1...8 {
+            
+            //Get a random number
+            let randomNumber = arc4random_uniform(13) + 1
+            
+            //Log the number
+            print(randomNumber)
+            
+            //Create the first card object
+            let cardOne = Card()
+            cardOne.imageName = "card\(randomNumber)"
+            
+            generatedCardsArray.append(cardOne)
+            
+            //Create the second card object
+            let cardTwo = Card()
+            cardTwo.imageName = "card\(randomNumber)"
+            
+            generatedCardsArray.append(cardTwo)
+            
+            //OPTIONAL: Make it so we only have unique pairs of cards
+            
+        }
+        
+        //TODO: Randomize the array
+        
+        
+        //Return the arry
+        return generatedCardsArray
+    }
     
 }
